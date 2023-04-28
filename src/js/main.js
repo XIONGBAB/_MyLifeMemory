4e5e882617656f9ddf39aac962b865679aa3d0ba
XIONGBAB <2770949518@qq.com>
2023-04-28 19:56:58 +0800

    add timer js

--- src/js/main.js
+++ src/js/main.js
@@ -23,49 +23,56 @@ if ("serviceWorker" in navigator) {
             });
     });
 }
+// rotate timer
 
-// JavaScript
-let navList = document.querySelectorAll(".navBanner ul li");
-navList.forEach(item => {
-    item.addEventListener("mouseenter", () => {
-        item.querySelector("a").classList.add("navBorder");
-    });
-    item.addEventListener("click", () => {
-        item.classList.add("navCircle");
-        navList.forEach(sibling => {
-            if (sibling !== item) {
-                sibling.classList.remove("navCircle");
-            }
+window.addEventListener("load", function () {
+    function updateTime() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, "0");
+        const minutes = now.getMinutes().toString().padStart(2, "0");
+        const seconds = now.getSeconds().toString().padStart(2, "0");
+
+        const hourDegree = hours * 30 + minutes / 2;
+        const minuteDegree = minutes * 6 + seconds / 10;
+        const secondDegree = seconds * 6;
+
+        let timerArr = [hours, minutes, seconds];
+        let rotatingDegree = [hourDegree, minuteDegree, secondDegree];
+        const timerBox = document.querySelectorAll(".timerBox ul li span");
+        const pointers = document.querySelectorAll(".timeBox");
+
+        for (let i = 0; i < timerBox.length; i++) {
+            timerBox[i].innerHTML = timerArr[i];
+            pointers[i].style.transform = `rotate(${rotatingDegree[i]}deg)`;
+        }
+    }
+    const timerContainer = document.querySelector(".timerBox");
+    timerContainer.style.display = "block";
+    let opacitySpeed = 0;
+    let closeSetinterval = setInterval(() => {
+        opacitySpeed += 0.1;
+        if (opacitySpeed >= 1) {
+            opacitySpeed = 1;
+            clearInterval(closeSetinterval);
+        }
+        document.querySelector(".timerBox").style.opacity = opacitySpeed;
+    }, 100);
+
+    setInterval(updateTime, 1000);
+
+    // JavaScript
+    let navList = document.querySelectorAll(".navBanner ul li");
+    navList.forEach(item => {
+        item.addEventListener("mouseenter", () => {
+            item.querySelector("a").classList.add("navBorder");
+        });
+        item.addEventListener("click", () => {
+            item.classList.add("navCircle");
+            navList.forEach(sibling => {
+                if (sibling !== item) {
+                    sibling.classList.remove("navCircle");
+                }
+            });
         });
     });
 });
-function updateTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
-
-    // const lis = document.querySelectorAll(".contactBox ul li");
-    // lis.forEach((li, index) => {
-    //     console.log(index);
-    //     const span = li.querySelector("span");
-    //     const timeBox = li.querySelector(".timeBox");
-    //     span[0].textContent = `${hour}`;
-    //     span[1].textContent = `${minute}`;
-    //     span[2].textContent = `${second}`;
-    //     const degrees = index * 120 + hour * 30 + minute * 0.5;
-    //     timeBox.style.transform = `rotate(${degrees}deg)`;
-    // });
-    const lis = document.querySelectorAll("li");
-    console.log(lis);
-    lis.forEach((li, index) => {
-        const span = li.querySelector("span");
-        console.log(span);
-        const timeBox = li.querySelector(".timeBox");
-        span.innerText = `${hours}:${minutes}:${seconds}`;
-        const degrees = index * 120 + hours * 30 + minutes * 0.5;
-        timeBox.style.transform = `rotate(${degrees}deg)`;
-    });
-}
-console.log(updateTime());
-// setInterval(updateTime, 1000);
