12608f004c14055604e3c1a5a17370571c1a5693
XIONGBAB <2770949518@qq.com>
2023-05-15 23:40:06 +0800

    new modify

--- src/js/memory.js
+++ src/js/memory.js
@@ -2,3 +2,19 @@ import "../css/memory.css";
 import "../css/iconfont.css";
 import "../css/normalize.css";
 import "../js/flexible.js";
+
+// js
+let rightBackground = document.querySelector(".rightBackground");
+let rightContent = document.querySelector(".rightContent");
+document.querySelector("#icon-link").href = require("../images/favicon.ico");
+
+document.querySelector(".goButton").addEventListener("click", () => {
+    rightBackground.style.opacity = 0;
+    setTimeout(() => {
+        rightBackground.style.display = "none";
+        setTimeout(() => {
+            rightContent.style.opacity = 1;
+            rightBackground.remove(); // 自杀
+        }, 500);
+    }, 500);
+});
