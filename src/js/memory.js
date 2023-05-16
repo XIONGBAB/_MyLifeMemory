b3c464c1ebfed7a40932f98a9d3b4b67d0a94f7b
XIONGBAB <2770949518@qq.com>
2023-05-17 00:29:00 +0800

    modify nav

--- src/js/memory.js
+++ src/js/memory.js
@@ -3,11 +3,12 @@ import "../css/iconfont.css";
 import "../css/normalize.css";
 import "../js/flexible.js";
 
-// js
-let rightBackground = document.querySelector(".rightBackground");
-let rightContent = document.querySelector(".rightContent");
+// icon js
 document.querySelector("#icon-link").href = require("../images/favicon.ico");
 
+// goButton js
+let rightBackground = document.querySelector(".rightBackground");
+let rightContent = document.querySelector(".rightContent");
 document.querySelector(".goButton").addEventListener("click", () => {
     rightBackground.style.opacity = 0;
     setTimeout(() => {
@@ -18,4 +19,24 @@ document.querySelector(".goButton").addEventListener("click", () => {
             rightBackground.remove(); // 自杀
         }, 500);
     }, 500);
+});
+
+// left nav js
+let navSubMenuBox = document.querySelectorAll(".navSubMenu a");
+let secondMenuBox = document.querySelectorAll(".secondMenuBox");
+let secondMenuLiBox = document.querySelectorAll(".subMenu");
+navSubMenuBox.forEach(item => {
+    item.addEventListener("click", () => {
+        navSubMenuBox.forEach(otherItem => {
+            if (otherItem != item) {
+                otherItem.classList.remove("active");
+            }
+        });
+        item.classList.add("active");
+        secondMenuBox.forEach(secondBoxItem => {
+            let secondMenuBoxHight = secondMenuLiBox[0].clientHeight;
+            console.log(secondMenuLiBox.length);
+            secondBoxItem.style.height = secondMenuBoxHight * secondMenuLiBox.length + "px";
+        });
+    });
 });
