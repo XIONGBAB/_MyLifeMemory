ae9889001bda3d7d81ddf0cbcb982d88c95630fe
XIONGBAB <2770949518@qq.com>
2023-05-25 22:46:43 +0800

    modify new

--- src/js/memory.js
+++ src/js/memory.js
@@ -22,64 +22,93 @@ document.querySelector(".goButton").addEventListener("click", () => {
 });
 
 // left nav js
+/* 
+暂存  navSubMenu[i].id = "navSubMenu" + (i + 1);
+function addNavSubMenu() {}  addNavSubMenu();
+    // let MenuBoxHeight = [];
+// secondMenuBox.style.height = secondMenuBox.children.length * secondMenuBox.children[0].clientHeight + "px";
+*/
 
-function addNavSubFunction(variableOne, variableTwo, isTrue) {
-    let elementResultOne = document.querySelectorAll(`${variableOne}`);
-    let elementResultTwo = document.querySelectorAll(`${variableTwo}`);
-    elementResultOne.forEach(item => {
-        item.addEventListener("click", function () {
-            let lisElementTag = null;
-            let itemSum = item.nextElementSibling;
-            let lisVariable = null;
-            elementResultTwo.forEach(items => {
-                if (items != item) {
-                    items.style.height = "0px";
-                }
-            });
-            if (itemSum) {
-                itemSum.childNodes.forEach(nodeLists => {
-                    if (nodeLists.className) {
-                        lisElementTag = nodeLists.className;
-                    }
-                });
-                lisVariable = itemSum.querySelectorAll(`.${lisElementTag}`);
-                if (isTrue) {
-                    this.classList.add("active");
-                    elementResultOne.forEach(otherItem => {
-                        if (otherItem != item) {
-                            otherItem.classList.remove("active");
-                        }
-                    });
-                }
-                let elementID = null;
-                if (item.nextElementSibling) {
-                    elementID++;
-                    this.nextElementSibling.id = "secondMenuBox" + elementID;
-                    this.nextElementSibling.style.height = lisVariable.length * lisVariable[0].clientHeight + "px";
-                }
-            }
-            addNavSubFunction(".secondSubMenuATag", ".thirdMenuBox", false);
-            if (itemSum) {
-                let secondMenuBox1 = document.getElementById("secondMenuBox1");
-                secondMenuBox1.style.height = this.nextElementSibling.style.height;
-            }
-        });
-    });
-}
-addNavSubFunction(".navSubMenuATag", ".secondMenuBox", true, ".navSubMenu");
+// let navSubMenu = document.querySelectorAll(".navSubMenu");
+
+// for (let i = 0; i < navSubMenu.length; i++) {
+//     navSubMenu[i].addEventListener("click", function () {
+//         let navSubMenuATag = document.querySelectorAll(".navSubMenuATag");
+//         let secondMenuBox = this.querySelector(".secondMenuBox");
+//         // 点li 下面有ul的时候执行这段
+//         if (secondMenuBox) {
+//             secondMenuBox.style.height = "100px";
+//             secondMenuBox.addEventListener("click", function () {
+//                 secondMenuBox.style.height = "100px";
+//             });
+//         }
+//         // 点li 删除其他的activeRed类名
+//         for (let j = 0; j < navSubMenuATag.length; j++) {
+//             navSubMenuATag[j].classList.remove("activeRed");
+//         }
+//         // 点li 添加当前activeRed类名
+//         navSubMenuATag[i].classList.add("activeRed");
+
+//         // // let isActiveRed = this.querySelector(".activeRed");
+//         // // MenuBoxHeight.push(secondMenuBox.clientHeight + "px");
+//         // if (isActiveRed == null) {
+//         //     navSubMenuATag[i].classList.add("activeRed");
+//         //     secondMenuBox.style.height = secondMenuBox.children.length * secondMenuBox.children[0].clientHeight + "px";
+//         //     MenuBoxHeight.splice(1);
+//         // }
+//     });
+// }
+
+// function addNavSubFunction(variableOne, variableTwo, isTrue) {
+//     let elementResultOne = document.querySelectorAll(`${variableOne}`);
+//     let elementResultTwo = document.querySelectorAll(`${variableTwo}`);
+//     elementResultOne.forEach(item => {
+//         item.addEventListener("click", function () {
+//             let lisElementTag = null;
+//             let itemSum = item.nextElementSibling;
+//             let lisVariable = null;
+//             elementResultTwo.forEach(items => {
+//                 if (items != item) {
+//                     items.style.height = "0px";
+//                 }
+//             });
+//             if (itemSum) {
+//                 itemSum.childNodes.forEach(nodeLists => {
+//                     if (nodeLists.className) {
+//                         lisElementTag = nodeLists.className;
+//                     }
+//                 });
+//                 lisVariable = itemSum.querySelectorAll(`.${lisElementTag}`);
+//                 if (isTrue) {
+//                     this.classList.add("active");
+//                     elementResultOne.forEach(otherItem => {
+//                         if (otherItem != item) {
+//                             otherItem.classList.remove("active");
+//                         }
+//                     });
+//                 }
+//                 let elementID = null;
+//                 if (item.nextElementSibling) {
+//                     elementID++;
+//                     this.nextElementSibling.id = "secondMenuBox" + elementID;
+//                     this.nextElementSibling.style.height = lisVariable.length * lisVariable[0].clientHeight + "px";
+//                 }
+//             }
+//             addNavSubFunction(".secondSubMenuATag", ".thirdMenuBox", false);
+//             if (itemSum) {
+//                 let secondMenuBox1 = document.getElementById("secondMenuBox1");
+//                 secondMenuBox1.style.height = this.nextElementSibling.style.height;
+//             }
+//         });
+//     });
+// }
+// addNavSubFunction(".navSubMenuATag", ".secondMenuBox", true, ".navSubMenu");
 
 // function isClick(variableThird, variableTwo) {
 //     let elementResultThird = document.querySelectorAll(`${variableThird}`);
 //     elementResultThird.forEach(items => {
 //         items.addEventListener("click", function () {
 //             console.log(this.querySelector(variableTwo).style.height);
-//             if (this.querySelector(variableTwo).style.height != "0px") {
-//                 this.querySelector(variableTwo).style.height = "0px";
-//             }
-//         });
-//     });
-// }
-// isClick(variableThird, variableTwo);
 
 //left scroll js
 
@@ -93,3 +122,5 @@ navContainerScroll.addEventListener("scroll", () => {
         searchBg.style.opacity = "0";
     }
 });
+
+// style="height:100px"
